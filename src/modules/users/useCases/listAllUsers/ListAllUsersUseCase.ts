e32cc83be9f05226982324fde38199a8deec4ebb
Herbert Paes <hebert.lima@prestadores.uhgbrasil.com.br>
2021-10-06 15:14:26 -0300

    first commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,25 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error("This user does not exist");
+    }
+    if (!user.admin) {
+      throw new Error("Cannot access users list");
+    }
+    const admin = this.usersRepository.list();
+
+    return admin;
+  }
+}
+
+export { ListAllUsersUseCase };
